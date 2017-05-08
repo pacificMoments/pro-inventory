361d1d3b85102659b875dad658b58c55bd541396
Lance Andrews <lance.andrews@xpo.com>
2017-05-08 10:48:48 -0700

    several conflicing changes just for fun

---
+++ pro-inventory/src/com/xpo/ltl/proinventory/dto/ProRangeDelete.java
@@ -0,0 +1,28 @@
+package com.xpo.ltl.proinventory.dto;
+
+public class ProRangeDelete {
+
+	String proPrefix;
+	String proSuffix;
+	String proType;
+	
+	public String getProPrefix() {
+		return proPrefix;
+	}
+	public void setProPrefix(String proPrefix) {
+		this.proPrefix = proPrefix;
+	}
+	public String getProSuffix() {
+		return proSuffix;
+	}
+	public void setProSuffix(String proSuffix) {
+		this.proSuffix = proSuffix;
+	}
+	public String getProType() {
+		return proType;
+	}
+	public void setProType(String proType) {
+		this.proType = proType;
+	}
+
+}
